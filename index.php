ba6f783569e6d8b92dbd57c3e7824f87bad66e02
Tatyana <TVGoncharova@yandex.ru>
2018-08-05 19:57:09 +0300

    composer

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 
-function sum($a, $b)
-{
-    $s=$a + $b;
-    return "says that $a+$b= $s\n";
-}
-$a=10;
-$b=4;
-echo sum($a, $b);
+include "src/Sum.php";
+
+$sum=new TatyanaGoncharova\Sum();
+
+echo $sum->Sum(4,12);
---
+++ src/Sum.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace TatyanaGoncharova;
+
+class Sum
+{
+function sum($a, $b)
+{
+    $s=$a + $b;
+    return "says that $a+$b= $s\n";
+
+}
+}
